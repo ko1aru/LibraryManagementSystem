7321a3df1f430f69e31b0b7404d9d0c9be2e2720
Aakash D <83403149+ko1aru@users.noreply.github.com>
2023-08-13 18:04:17 +0530

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-class Book {
+class Book implements Comparable<Book> {
 	String title;
 	String author;
 	String isbn;
@@ -8,4 +8,8 @@ class Book {
 	String borrowedby;
 	Date borrowedDate;
 	Date dueDate;
+	
+	public int compareTo(Book other) {
+		return this.title.compareTo(other.title);
+	}	
 }
--- Library.java
+++ Library.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Library {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Calendar cal = Calendar.getInstance();
 		
 		List<User> userList = new ArrayList<>();
-		List<Book> bookList = new ArrayList<>();
+		Set<Book> bookList = new TreeSet<>();
+		
+		initializeBooks(bookList);
 		
 		while (true) {
 			System.out.println("Are you a New User or a Existing User?\n1. New user\n2. Existing user");
@@ -53,12 +54,12 @@ public class Library {
 									break;
 									
 								case 4:
-									borrowBook(bookList, user, sc, cal);
+									borrowBook(bookList, user, sc);
 									System.out.println("-----------------------------------------------------\n\n\n");
 									break;
 									
 								case 5:
-									returnBook(bookList, sc, cal);
+									returnBook(bookList, sc);
 									System.out.println("-----------------------------------------------------\n\n\n");
 									break;
 									
@@ -89,6 +90,28 @@ public class Library {
 		}
 	}
 	
+	public static void initializeBooks(Set<Book> bookList) {
+	    String[] titles = { "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice",
+	    	"The Hobbit", "Harry Potter and the Sorcerer's Stone", "The Catcher in the Rye",
+	    	"The Lord of the Rings", "Animal Farm", "Brave New World" };
+
+	    String[] authors = { "F. Scott Fitzgerald", "Harper Lee", "George Orwell", "Jane Austen",
+	    	"J.R.R. Tolkien", "J.K. Rowling", "J.D. Salinger", "J.R.R. Tolkien", "George Orwell",
+	        "Aldous Huxley" };
+
+	    String[] isbns = {"978-0743273565", "978-0061120084", "978-0451524935", "978-0141439518",
+	        "978-0618002214", "978-0590353427", "978-0316769488", "978-0618640157", "978-0451524935",
+	        "978-0060850524" };
+
+	    for (int i = 0; i < titles.length; i++) {
+	        Book book = new Book();
+	        book.title = titles[i];
+	        book.author = authors[i];
+	        book.isbn = isbns[i];
+	        bookList.add(book);
+	    }
+	}
+	
 	public static void createNewUser(List<User> userList, Scanner sc) {
 		User userObj = new User();
 		userObj.setId();
@@ -101,14 +124,24 @@ public class Library {
 		String dob = sc.nextLine();
 		userObj.setDob(dob);
 		
-		System.out.println("Select your Role:\n1. User\n2. Admin");
-		int role = sc.nextInt();
-		if (role==1) {
-			userObj.setRole("User");
-		} else if (role==2) {
-			userObj.setRole("Admin");
-		} else {
-			System.out.println("Invalid role choice");
+		boolean isValidRole = false;
+		while (!isValidRole) {
+			try {
+				System.out.println("Select your Role:\n1. User\n2. Admin");
+				int role = sc.nextInt();
+				if (role==1) {
+					userObj.setRole("User");
+					isValidRole = true;
+				} else if (role==2) {
+					userObj.setRole("Admin");
+					isValidRole = true;
+				} else {
+					System.out.println("Invalid role choice");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter '1' or '2'\n");
+				sc.nextLine();
+			}
 		}
 		
 		userList.add(userObj);		
@@ -117,7 +150,7 @@ public class Library {
 			
 	}
 	
-	public static void addBook(List<Book> bookList, User user, Scanner sc) {
+	public static void addBook(Set<Book> bookList, User user, Scanner sc) {
 		if (user.getRole().equals("Admin")) {
 			Book bookObj = new Book();
 			
@@ -134,11 +167,10 @@ public class Library {
 			
 			System.out.println("\nBook added!\nBook name: "+bookObj.title+"\nBook Author: "+bookObj.author+
 					"\nISBN: "+bookObj.isbn);
-		} else System.out.println("You need to be an admin to perform this operation");
-		
+		} else System.out.println("You need to be an admin to perform this operation");		
 	}
 	
-	public static void deleteBook(List<Book> bookList, User user, Scanner sc) {
+	public static void deleteBook(Set<Book> bookList, User user, Scanner sc) {
 		if (user.getRole().equals("Admin")) {
 			Iterator<Book> it = bookList.iterator();
 			boolean found = false;
@@ -157,42 +189,44 @@ public class Library {
 		} else System.out.println("You need to be an admin to perform this operation");
 	}
 	
-	public static Book findBook(List<Book> bookList, Scanner sc) {
-		Book book = new Book();
+	public static Book findBook(Set<Book> bookList, Scanner sc) {
 		System.out.println("Select search criteria\n1. Title\n2. Author");
 		try {
 			int criteria = sc.nextInt();
 			sc.nextLine();
+			
 			switch (criteria) {
 			case 1:
 				System.out.println("Enter the title: ");
 				String title = sc.nextLine();			
 				
-				for (Book x : bookList) {
-					if (title.toLowerCase().contains(x.title)) {
+				for (Book book : bookList) {
+					if (title.equalsIgnoreCase(book.title)) {
 						System.out.println("\nBook found!");
-						book = x;
 						System.out.println("\nBook name: "+book.title+"\nAuthor: "+book.author+"\n");
-					} else System.out.println("Book not found\n\n");
+						return book;
+					}
 				}
-				return book;
-				
+				System.out.println("Book not found\n");
+				return null;
+					
 			case 2:
 				System.out.println("Enter the Author's name: ");
 				String authorName = sc.nextLine();
 				
-				for (Book x : bookList) {
-					if (authorName.equalsIgnoreCase(x.author)) {
+				for (Book book : bookList) {
+					if (authorName.equalsIgnoreCase(book.author)) {
 						System.out.println("Author found!");
-						book = x;
 						System.out.println("Book name: "+book.title+"\nAuthor: "+book.author);
-					} else System.out.println("Author not found\n\n");
+						return book;
+					}
 				}
-				return book;
+				System.out.println("Author not found\n");
+				return null;
 				
 			default:
 				System.out.println("Invalid choice!\n\n");
-				return book;
+				return null;
 			}
 		} catch (InputMismatchException e1) {
 			System.out.println("Please enter '1' or '2'");
@@ -201,11 +235,11 @@ public class Library {
 		}
 	}
 	
-	public static void displayBooks(List<Book> bookList) {
-		System.out.println("Title               Author's name                Status\n");
+	public static void displayBooks(Set<Book> bookList) {
+		System.out.println("Title                      Author's name                Status\n");
 		for(Book x : bookList) {
 			System.out.print(x.title);
-			for (int i=0; i<20-x.title.length(); i++) {
+			for (int i=0; i<27-x.title.length(); i++) {
 				System.out.print(" ");
 			}
 			System.out.print(x.author);
@@ -216,20 +250,24 @@ public class Library {
 		}
 	}
 	
-	public static void borrowBook(List<Book> bookList, User user, Scanner sc, Calendar cal) {
+	public static void borrowBook(Set<Book> bookList, User user, Scanner sc) {
+		Calendar cal = Calendar.getInstance();
 		Book book = findBook(bookList, sc);
-		if (!book.isBorrowed) {
-			book.isBorrowed = true;
-			book.borrowedby = user.getName();
-			book.borrowedDate = cal.getTime();
-			cal.add(Calendar.DAY_OF_MONTH, 15);
-			book.dueDate = cal.getTime();
-			System.out.println("Book borrowed\nUser name: "+book.borrowedby+"\nBook name: "+book.title+
-					"\nBorrowed On: "+book.borrowedDate+"\nDUE DATE: "+book.dueDate+"\n\n");
-		} else System.out.println("Book not available\n\n");
+		if (book != null) {
+			if (!book.isBorrowed) {
+				book.isBorrowed = true;
+				book.borrowedby = user.getName();
+				book.borrowedDate = cal.getTime();
+				cal.add(Calendar.DAY_OF_MONTH, 15);
+				book.dueDate = cal.getTime();
+				System.out.println("Book borrowed\nUser name: "+book.borrowedby+"\nBook name: "+book.title+
+						"\nBorrowed On: "+book.borrowedDate+"\nDUE DATE: "+book.dueDate+"\n\n");
+			} else System.out.println("Book not available\n\n");
+		} else System.out.println("Please enter a valid book name from the list");
 	}
 	
-	public static void returnBook(List<Book> bookList, Scanner sc, Calendar cal) {
+	public static void returnBook(Set<Book> bookList, Scanner sc) {
+		Calendar cal = Calendar.getInstance();
 		Book book = findBook(bookList, sc);
 		int price = 2;
 		int fine = 1;
@@ -239,6 +277,7 @@ public class Library {
 					"\nPrice for 1 day is Rs.2\nFine after due date is Rs.1 per day\n");
 			
 			Date today = cal.getTime();
+			System.out.println("Current time and day: "+today);
 			long diff = (today.getTime() - book.borrowedDate.getTime()) / (24*60*60*1000);
 			
 			if(today.compareTo(book.dueDate)<=0) {
